41e8d674dd183dab31de3e58b7726c88916f7753
Robert Hughes <roberts.ginger.email@gmail.com>
2018-04-29 08:54:51 +0100

    use rspec and add user model

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ group :development do
 end
 
 group :test do
+  gem 'rspec-rails'
   gem 'capybara', '>= 2.15', '< 4.0'
   gem 'selenium-webdriver'
   gem 'chromedriver-helper'
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe User, type: :model do
+  let(:user) { User.new(username: 'rob') }
+  let(:duplicate_user) { User.new(username: 'rob') }
+
+  it 'creates new users' do
+    expect(user).to be_valid
+  end
+
+  it 'has a unique #username' do
+    user.save
+    expect(duplicate_user).to_not be_valid
+  end
+end
