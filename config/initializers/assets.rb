8ec80147be519b447c75a0c125eae5f31d35b31c
Robert Hughes <roberts.ginger.email@gmail.com>
2018-05-01 20:00:06 +0100

    start styling and add custom fonts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join('app',
+                                                         'assets',
+                                                         'fonts')
