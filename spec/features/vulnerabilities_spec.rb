58d71d2655fff81904394cd4564f20e912b8e545
Robert Hughes <roberts.ginger.email@gmail.com>
2018-05-12 15:49:25 +0100

    add table for certificates and styling

--- spec/features/vulnerabilities_spec.rb
+++ spec/features/vulnerabilities_spec.rb
@@ -3,21 +3,23 @@
 require 'rails_helper'
 
 RSpec.describe 'certificates' do
-  it 'lists out all of the certificates with their vulnerabilities' do
-    awesome_site = {
+  before do
+    @awesome_site = Certificate.create(
       url: 'awesome-site.com',
-      expiration: 'Oct 2015',
-      environment: 'Headend',
-      vulnerabilities: "Expired\nDES",
-      ciphers: "DES\n3DES\nSHA-1",
-      ip_address: '123.123.123.123',
-      load_balancer: 'stm01.che.room101.com'
-    }
+      expiration: Date.iso8601('2016-07-03'),
+      ip_address: '123.123.123.123'
+    )
+    @awesome_site.ciphers.create(name: 'DES', secure: false)
+    @awesome_site.ciphers.create(name: '3DES', secure: false)
+    @awesome_site.ciphers.create(name: 'SHA-1', secure: true)
+    @awesome_site.create_load_balancer(hostname: 'stm01.che.room101.com',
+                                       ip_address: '10.207.7.123')
+  end
+
+  it 'lists out all of the certificates with their vulnerabilities' do
     visit '/certificates'
     within 'table' do
-      awesome_site.each do |_heading, text|
-        expect(page.body).to have_text(text)
-      end
+      expect(page.body).to have_text(@awesome_site.url)
     end
   end
 end
