cf41720f4f38a24144bbba78f772576ebebcfd73
Robert Hughes <roberts.ginger.email@gmail.com>
2018-05-12 07:08:47 +0100

    refactor certificate spec model

--- spec/models/certificate_spec.rb
+++ spec/models/certificate_spec.rb
@@ -3,15 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe Certificate do
-  before do
-    @load_balancer = LoadBalancer.create(hostname: 'a', ip_address: '1')
-  end
-
   let(:certificate) do
     Certificate.new(
       url: 'site.com',
       expiration: Date.iso8601('2016-06-29'),
-      load_balancer_id: @load_balancer.id,
       ip_address: '123.123.123.123'
     )
   end
@@ -21,6 +16,8 @@ RSpec.describe Certificate do
   end
 
   it 'has a load balancer hostname' do
+    certificate.save
+    certificate.create_load_balancer(hostname: 'a', ip_address: '1')
     expect(certificate.load_balancer_hostname).to eql(@load_balancer.hostname)
   end
 end
