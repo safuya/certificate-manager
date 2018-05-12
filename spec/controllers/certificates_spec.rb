b0ef9224543882ef2ce1d80b7731b193c2cd28cb
Robert Hughes <roberts.ginger.email@gmail.com>
2018-05-12 08:30:40 +0100

    fix provides all certificates controller test

--- spec/controllers/certificates_spec.rb
+++ spec/controllers/certificates_spec.rb
@@ -6,19 +6,17 @@ require 'date'
 RSpec.describe CertificatesController do
   before do
     @user = User.create(username: 'rob', password: 'letmein')
-    load_balancer = LoadBalancer.create(hostname: 'lb01.bteat.room101.com',
-                                        ip_address: '10.101.20.5')
     @certificate = Certificate.create(url: 'site.com',
                                       expiration: Date.iso8601('2016-06-29'),
-                                      load_balancer_id: load_balancer.id,
                                       ip_address: '123.123.123.123')
-    cipher = Cipher.create(name: 'TLS_DHE_RSA_WITH_AES_128_CBC_SHA',
-                           secure: true,
-                           certificate_ids: [@certificate.id])
+    @certificate.ciphers.create(name: 'TLS_DHE_RSA_WITH_AES_128_CBC_SHA',
+                                secure: true)
+    @certificate.create_load_balancer(hostname: 'lb01.bteat.room101.com',
+                                      ip_address: '10.101.20.5')
   end
 
   it 'provides all certificates' do
     get :index, session: { user_id: @user.id }
-    expect(assigns(:certificates)).to eql([@certificate])
+    expect(assigns(:certificates).first).to eql(@certificate)
   end
 end
