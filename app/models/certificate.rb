7d591588696903a515cd4368314b7a4cdd5e12f2
Robert Hughes <roberts.ginger.email@gmail.com>
2018-05-13 14:48:09 +0100

    intermediate point

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -33,6 +33,7 @@ class Certificate < ApplicationRecord
 
   def self.load_balancer_search(search_term)
     binding.pry
+    select(Arel.star).where(LoadBalancer[:hostname]).joins(:load_balancer)
     joins(:load_balancer)
       .where('load_balancers.hostname like ?', "%#{search_term}%")
   end
