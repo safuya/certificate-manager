562eb89064ae1d9700277279c5e92c4e674a13be
Robert Hughes <roberts.ginger.email@gmail.com>
2018-05-19 08:51:47 +0100

    add link to add new certificate from index

--- spec/features/add_certificates_spec.rb
+++ spec/features/add_certificates_spec.rb
@@ -3,7 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe 'add_certificates' do
-  xit 'links to adding certificates on the certificates index' do
+  before do
+    @user = User.create(username: 'rob', password: 'letmein')
+  end
+
+  it 'links to adding certificates on the certificates index' do
+    page.set_rack_session(user_id: @user.id)
     visit '/certificates'
+    click_link 'add-certificate'
+    expect(page.body).to have_text('Add Certificate')
+  end
+
+  it 'lets you fill in all the credentials' do
+    page.set_rack_session(user_id: @user.id)
+    visit '/certificates/new'
   end
 end
