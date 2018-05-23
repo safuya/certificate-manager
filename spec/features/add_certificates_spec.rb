71ed16c5bc42cb3ed10053c66d09b3b114f47e40
Robert Hughes <roberts.ginger.email@gmail.com>
2018-05-23 06:18:48 +0100

    refactor new and edit cert into partial

--- spec/features/add_certificates_spec.rb
+++ spec/features/add_certificates_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe 'add_certificates' do
     fill_in :certificate_ip_address, with: '1.2.3.4'
     fill_in :certificate_load_balancer_hostname, with: 'lb01.room101.com'
     check :certificate_cipher_ids_1
-    click_button 'add-certificate'
+    click_button 'submit-certificate'
     expect(page.current_url).to eql(certificates_url)
     expect(page.body).to have_text('website.com')
   end
--- spec/features/edit_certificates_spec.rb
+++ spec/features/edit_certificates_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe 'edit_certificates' do
     visit '/certificates'
     click_link "edit-#{@certificate.id}"
     fill_in :certificate_load_balancer_hostname, with: 'lb01.room101.com'
-    click_button 'edit-certificate'
+    click_button 'submit-certificate'
     expect(page.current_url).to eql(certificates_url)
     expect(page.body).to have_text('lb01.room101.com')
   end
